8e15c7bde1438a7fddcd55991bacf81626795e78
Alex <atomasllimos@gmail.com>
2024-01-14 14:37:06 +0100

    feat: Add tailwind-typescript-expo boilerplate

---
+++ tailwind-typescript-expo/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledSafeArea = styled(SafeAreaView);
+const StyledText = styled(Text);
+
+export default function App() {
+    return (
+        <StyledSafeArea className='flex bg-[#fff] items-center justify-center'>
+            <StyledText className='text-red-600 text-4xl text-center p-5'>Expo With TypeScript and Tailwind CSS</StyledText>
+        </StyledSafeArea>
+    );
+}
